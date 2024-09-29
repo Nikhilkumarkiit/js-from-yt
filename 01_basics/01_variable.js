cf01774ebeb489a408af9f3c3fb58f2f76fc729b
Nikhilkumarkiit <139627559+Nikhilkumarkiit@users.noreply.github.com>
2024-09-29 12:54:14 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,6 @@
+const customerId=98765
+let accountMail="nk@gmail.com"
+var accountPassword="ram"
+accountMail="kk@gamil.com"
+/*prefer not to use var because of issue in block scope and functional scope*/
+console.table([customerId,accountMail,accountPassword])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; 
+// treat all js code as newer version
+// alert(3+3) we are using nodejs,not browser
+let data=4
+let name ="hitesh"
+let isLoggedIn=false
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value it is a object
+// undefined =>
+// symbol=>unique
+// object
+console.log( typeof data);
+console.log( typeof undefined);
+// undefined
+console.log( typeof null);
+// object
+
+
