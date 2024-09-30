1b4d4fcdc43f8b69b58490b5b767e8d617a01092
Nikhilkumarkiit <139627559+Nikhilkumarkiit@users.noreply.github.com>
2024-09-30 08:24:39 +0000

    conversion of data types

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,22 @@
+let score="33"
+console.log (typeof (score));
+let num=Number(score)
+console.log(typeof num);
+let score2="33nik"
+console.log(score2);
+console.log (typeof (score2));
+let num2=Number(score2)
+console.log(num2)
+console.log(typeof num2);
+// agar number mai convert nhi hota hai to nan ya 0 deta hai
+// "33" => 33
+// "33abc" => NAN
+// true =>1 ;false=>0
+// "" =>false  "nikhil"=>true
+let bool=1
+let boolnumber=Boolean(bool)
+console.log(boolnumber);
+let string=33
+let numbertostring=String(string)
+console.log(numbertostring)
+console.log(typeof numbertostring)
