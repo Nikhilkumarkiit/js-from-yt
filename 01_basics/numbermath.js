cedda67fa3c0c5bba273d9564882ded76654f814
Nikhilkumarkiit <139627559+Nikhilkumarkiit@users.noreply.github.com>
2024-09-30 18:15:29 +0000

    math &num

---
+++ 01_basics/numbermath.js
@@ -0,0 +1,24 @@
+const val1=100
+// console.log(val1)
+const val2=new Number(100)
+// console.log(val2)
+// console.log(val1.toString().length)
+// here we convert it to string to increase its property
+const num=123.9345
+const num2=10000000000
+// console.log(num.toFixed(2))
+// console.log(num.toPrecision(2))
+// console.log(num2.toLocaleString('en-IN'))
+// ****** Maths *********
+// console.log(Math.abs(-9))
+// console.log(Math.round(9.5))
+// console.log(Math.ceil(8.1))
+// console.log(Math.floor(8.9))
+// console.log(Math.min(6,9,88,0))
+// console.log(Math.max(6,9,88,0))
+console.log((Math.random()*10)+1)
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+ 
