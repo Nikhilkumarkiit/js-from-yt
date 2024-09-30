711cb804e434c701cb98bdb21e0515d627709219
Nikhilkumarkiit <139627559+Nikhilkumarkiit@users.noreply.github.com>
2024-09-30 13:47:48 +0000

    heap stack

--- 01_basics/notes.js
+++ 01_basics/notes.js
@@ -17,4 +17,17 @@ const myfun=function(){
 }
 // non primitive ka type object ata hai function ka function object ata hai
 
+//  MEMORY
+//  stack(primitive),heap(non-primitive)
+let user1={
+    email:"kk@gmail.com",
+    upii:"ram@ybl"
+}
+let user2=user1
+user2.email="nk@gmail.com"
+console.log(user1)
+console.log(user2)
+// object non primitive hai uska value reference se pass hota hai to change nhi hota hai original milta hai
+// primitive value ka value copy jata hai jo update hota hai copy milta hai
+
 
