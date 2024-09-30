15dc4eea80bc02eea68b47ff3f3a2c9e35e7165b
Nikhilkumarkiit <139627559+Nikhilkumarkiit@users.noreply.github.com>
2024-09-30 11:57:21 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,7 @@
+console.log("2">1)
+//  here it treats 2 as number not as string
+console.log(null>0)
+console.log(null>=0)
+// here comparison operator convert NULL into number 0
+console.log("2"===2)
+// here=== not only check value but also check data types== make it true but === result as false
