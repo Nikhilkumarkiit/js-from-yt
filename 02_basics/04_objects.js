72567de029c3a55e528d2a542db954e30a492519
Nikhilkumarkiit <139627559+Nikhilkumarkiit@users.noreply.github.com>
2024-10-01 08:13:11 +0000

    obj

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,33 @@
+ const web1={}
+//  non-singleton
+const web2=new Object()
+// singleton
+const tinderuser={}
+tinderuser.name="nikhil"
+tinderuser.id="123erf"
+// console.log(tinderuser)
+ const regular={
+    email:"kk@gmail.com",
+    fullname:{
+        username:{
+            firstname:"nikhil",
+            lastname:"kumar"
+        }
+    }
+ }
+//  console.log(regular.fullname)
+//  console.log(regular.fullname.username.firstname)
+ const obj1={1:"a", 2:"b"}
+ const obj2={3:"g",4:"p"}  
+//  const obj3={obj1,obj2}
+//  aise direct karne se ye hoga ki tum obj ke andar obj bana doge
+//  console.log(obj3)
+const obj_3=Object.assign({},obj1,obj2)
+// ek saath ho jayega par ek obj ke andar ek nhi hoga
+console.log(obj_3)
+const obj3_2={...obj1,...obj2}
+// spread technique
+// console.log(obj3_2)
+console.log(Object.values(tinderuser))
+console.log(Object.keys(tinderuser))
+console.log(Object.entries(tinderuser))
