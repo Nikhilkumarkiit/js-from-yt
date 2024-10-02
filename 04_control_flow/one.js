13e74723c68bd969de0e7083e5f534ccb5e41c17
Nikhilkumarkiit <139627559+Nikhilkumarkiit@users.noreply.github.com>
2024-10-02 05:06:56 +0000

    if else switch

---
+++ 04_control_flow/one.js
@@ -0,0 +1,15 @@
+// simple if else and switch case
+// falsy values false,0,-0,BigInt 0n,"",null,undefined,NaN
+// string ke andar 0 true hai,empty array,object true hai
+// NULLISH coalescing operator(??):null undefined
+let val1;
+val1=5 ?? 10
+val2=null ?? 10
+console.log(val1)
+val3=null ?? 10 ?? 20
+// yaha agar null aya to aage wale value ko lo
+// TERNIARY OPERATOR
+// condition ? true : false
+const price=100
+price<=200 ? console.log("budget friendly"):console.log("costly")
+ 
