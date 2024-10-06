b21835af676838f56199ac6a6212f421cc517996
Nikhilkumarkiit <139627559+Nikhilkumarkiit@users.noreply.github.com>
2024-10-06 08:43:27 +0000

    for each

--- 05_iteration/2.js
+++ 05_iteration/2.js
@@ -1,1 +1,38 @@
 const coding=["cpp","java","python","ruby"]
+coding.forEach( function(val){
+    // console.log(val)
+  
+})
+// call back use kar rha hai call back mai naam nhi hota function ka
+coding.forEach( (val)=>{
+    // console.log(val)
+})
+// ye bhi call back kar rha hai functon par arrow function use karke
+function printme(val)
+{
+    // console.log(val)
+}
+coding.forEach(printme)
+coding.forEach((item,index,arr)=>{
+    // console.log(item,index,arr)
+})
+const mycoding=[
+    {
+        languagename:'c++',
+        languagefile:".cpp"
+
+    },
+    {
+        languagename:'javascript',
+        languagefile:".js"
+
+    },
+    {
+        languagename:'phyton',
+        languagefile:".py"
+
+    },
+]
+mycoding.forEach((item)=>{
+    console.log(item.languagefile)
+})
